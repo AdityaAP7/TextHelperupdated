5e0fb388a5b957fad5a2e47183a3f18b590afaca
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2026-02-04 20:36:57 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,7 +20,7 @@ const handleLoClick = ()=>{
     const handleClearClick = ()=>{ 
         let newText = '';
         setText(newText);
-        props.
+        
 
     const handleOnChange = (event)=>{
         setText(event.target.value) 
